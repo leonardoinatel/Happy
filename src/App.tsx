75ab9d2cddcd9bbcc3b371f117f8e7bdc0261f49
leonardoinatel <leonardodamasceno@gec.inatel.br>
2020-10-13 14:53:06 -0300

    first commit nlw3

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/global.css';
+
+interface TitleProps {
+  text: string,
+}
+
+const Title = (props: TitleProps) => {
+  return (
+    <h1>{props.text}</h1>
+  )
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="page-landing">
+      <Title  text="Titulo 1"/>
+      <Title  text="Titulo 2"/>
+      <Title  text="Titulo 3"/>
+      <Title  text="Titulo 4"/>
     </div>
   );
 }
